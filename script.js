8d9235dbf2d45c6cfb3c7331dc4569b7660da1bd
Andreas Hölzl <andy.woodly@googlemail.com>
2014-01-06 23:08:17 +0100

    base ui, player dnd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 angular.module('toptrumps', [])
 
-.controller('MainCtl', ['$scope', function($scope) {
+.controller('MainCtl', ['$scope', '$http', function($scope, $http) {
 
     $scope.button = {
         text: "Play"
@@ -18,5 +18,96 @@ angular.module('toptrumps', [])
         losses: 0
     };
 
+    $scope.play = function() {
+        alert("play");
+    };
 
-}]);
+    $scope.canPlay = true;
+
+    function init() {
+        $http.get("data/data.json")
+            .success(function(data) {
+                $scope.data = data;
+                if (angular.isDefined(data)) {
+                    console.log("got card data");
+                }
+            })
+            .error(function(data, status) {
+                alert("Failed to load card data, "+status);
+            });
+    }
+
+    init();
+
+    $scope.$on("filedrop", function(event, data) {
+        console.log("loading player", data.id);
+        var r = new FileReader();
+        r.onload = function(e) {
+            var source = e.target.result;
+            try {
+                var player = eval(source);
+                player.wins = 0;
+                player.losses = 0;
+                $scope[data.id] = player;
+                $scope.$apply();
+            } catch (e) {
+                alert("Failed to parse player source.");
+                console.log(e);
+            }
+        };
+        r.onerror = function(error) {
+            alert("Failed to load dropped file.");
+            console.log(error);
+        };
+        r.readAsText(data.file);
+    });
+
+}])
+
+.directive('dropZone', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'dropzone.tpl.html',
+        scope: {
+            id: '='
+        },
+        link: function(scope, el, attrs, controller) {
+            var id = scope.id;
+
+            el.bind("dragover", function(e) {
+                if (e.preventDefault) {
+                    e.preventDefault();
+                }
+
+                e.dataTransfer.dropEffect = 'move';
+                return false;
+            });
+
+            el.bind("dragenter", function(e) {
+                angular.element(e.target).addClass('drag-over');
+            });
+
+            el.bind("dragleave", function(e) {
+                angular.element(e.target).removeClass('drag-over');
+            });
+
+            el.bind("drop", function(e) {
+                angular.element(e.target).removeClass('drag-over');
+                if (e.preventDefault) {
+                    e.preventDefault(); // Necessary. Allows us to drop.
+                }
+
+                if (e.stopPropogation) {
+                    e.stopPropogation(); // Necessary. Allows us to drop.
+                }
+
+                var dt = e.dataTransfer;
+                var files = dt.files;
+
+                scope.$emit("filedrop", { id: id, file: files[0] });
+                return false;
+            });
+
+        }
+    }
+});
---
+++ simplePlayer.js
@@ -0,0 +1,28 @@
+(function() {
+    var player = {
+        name: "Random Player",
+
+        init: function(completeCardData) {
+            // do nothing
+        },
+
+        ask: function(myCard) {
+            // ask random category
+            if (!myCard || !myCard.values) {
+                throw new Error("Ill formatted card", myCard);
+            }
+            var nrOfValues = myCard.values.length;
+            var idx = Math.floor(Math.random() * nrOfValues);
+            return { idx: idx, value: myCard.values[idx] };
+        },
+
+        cardWon: function(myCard, otherCard) {
+            // do nothing
+        },
+
+        cardLost: function(myCard, otherCard) {
+            // do nothing
+        }
+    };
+    return player;
+})();
