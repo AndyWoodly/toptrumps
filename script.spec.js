05d10bc646ba9f590c17e5799c45f1de380ffbcc
Andreas Hölzl <andy.woodly@googlemail.com>
2014-01-11 10:20:06 +0100

    testing with spies

--- script.spec.js
+++ script.spec.js
@@ -1,52 +1,47 @@
 describe("toptrumps: ", function() {
 
-    var gameService;
+    var gameService, data, Player;
 
     beforeEach(module('toptrumps'));
 
     beforeEach(function() {
+
         inject(function($injector) {
             gameService = $injector.get('GameService');
         });
+
+        data = {
+            cards: [
+                {
+                    "name": "Winner",
+                    "values": [ 11, 22 ]
+                },
+                {
+                    "name": "Looser",
+                    "values": [ 1, 2 ]
+                }
+            ]
+        };
+
+        Player = function () {
+            this.wins = 0;
+            this.losses = 0;
+            this.init = jasmine.createSpy();
+            this.ask = function(myCard) {
+                return {
+                    idx: 0,
+                    value: myCard.values[0]
+                };
+            };
+            this.cardWon = jasmine.createSpy();
+            this.cardLost = jasmine.createSpy();
+        };
+
     });
 
     describe("GameService -", function() {
 
-        it("should work correctly", function() {
-            var data = {
-                cards: [
-                    {
-                        "name": "Winner",
-                        "values": [ 11, 22, 33, 44, 55 ]
-                    },
-                    {
-                        "name": "Looser",
-                        "values": [ 1, 2, 3, 4, 5 ]
-                    }
-                ]
-            };
-            var Player = function () {
-                this.wins = 0;
-                this.losses = 0;
-                this.init = function(data) {
-                    this.data = data;
-                };
-
-                this.ask = function(myCard) {
-                    return {
-                        idx: 0,
-                        value: myCard.values[0]
-                    };
-                };
-
-                this.cardWon = function(myCard, otherCard) {
-                    this.cardWon = otherCard;
-                };
-
-                this.cardLost = function(myCard, otherCard) {
-                    this.cardLost = myCard;
-                }
-            };
+        it("should interact with players correctly", function() {
 
             var playerA = new Player();
             var playerB = new Player();
@@ -54,11 +49,17 @@ describe("toptrumps: ", function() {
             game.setIterations(1);
             game.play();
 
-            expect(playerA.data).toBe(data);
-            expect(playerB.data).toBe(data);
+            expect(playerA.init).toHaveBeenCalledWith(data);
+            expect(playerB.init).toHaveBeenCalledWith(data);
+
+            expect(playerA.cardWon).toHaveBeenCalledWith(data.cards[0],data.cards[1]);
+            expect(playerA.cardLost).not.toHaveBeenCalled();
+            expect(playerB.cardLost).toHaveBeenCalledWith(data.cards[1],data.cards[0]);
+            expect(playerB.cardWon).not.toHaveBeenCalled();
 
             expect(playerA.wins).toBe(1);
             expect(playerA.losses).toBe(0);
+
             expect(playerB.wins).toBe(0);
             expect(playerB.losses).toBe(1);
         });
